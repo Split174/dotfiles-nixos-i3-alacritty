d6dddd17723fbb4ac0dbebc3241f6c129011adf8
Split174 <sergei.popov174@gmail.com>
2024-10-07 03:35:09 +0500

    refactiong config with sonnet3.5

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -1,6 +1,6 @@
 # Edit this configuration file to define what should be installed on
 # your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
+# and in the NixOS manual (accessible by running 'nixos-help').
 
 { config, pkgs, ... }:
 
@@ -10,76 +10,65 @@
       ./hardware-configuration.nix
     ];
 
+  # System Configuration
+  system.stateVersion = "24.05";
   nixpkgs.config.allowUnfree = true;
-  
-  # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-
   environment.pathsToLink = [ "/libexec" ]; # links /libexec from derivations to /run/current-system/sw 
 
-  networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
-  networking.networkmanager.enable = true;
-
-  # Set your time zone.
-  time.timeZone = "Asia/Yekaterinburg";
-
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
-
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "ru_RU.UTF-8";
-    LC_IDENTIFICATION = "ru_RU.UTF-8";
-    LC_MEASUREMENT = "ru_RU.UTF-8";
-    LC_MONETARY = "ru_RU.UTF-8";
-    LC_NAME = "ru_RU.UTF-8";
-    LC_NUMERIC = "ru_RU.UTF-8";
-    LC_PAPER = "ru_RU.UTF-8";
-    LC_TELEPHONE = "ru_RU.UTF-8";
-    LC_TIME = "ru_RU.UTF-8";
+  # Boot Configuration
+  boot.loader = {
+    systemd-boot.enable = true;
+    efi.canTouchEfiVariables = true;
   };
 
-  # Enable the X11 windowing system.
-  #services.xserver.enable = true;
+  # Networking
+  networking = {
+    hostName = "nixos"; # Define your hostname.
+    networkmanager.enable = true;
+    # wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+    # proxy.default = "http://user:password@proxy:port/";
+    # proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+  };
 
-  # Enable the XFCE Desktop Environment.
-  #services.xserver.displayManager.lightdm.enable = true;
-  #services.xserver.desktopManager.xfce.enable = true;
+  # Time and Locale
+  time.timeZone = "Asia/Yekaterinburg";
+  i18n = {
+    defaultLocale = "en_US.UTF-8";
+    extraLocaleSettings = {
+      LC_ADDRESS = "ru_RU.UTF-8";
+      LC_IDENTIFICATION = "ru_RU.UTF-8";
+      LC_MEASUREMENT = "ru_RU.UTF-8";
+      LC_MONETARY = "ru_RU.UTF-8";
+      LC_NAME = "ru_RU.UTF-8";
+      LC_NUMERIC = "ru_RU.UTF-8";
+      LC_PAPER = "ru_RU.UTF-8";
+      LC_TELEPHONE = "ru_RU.UTF-8";
+      LC_TIME = "ru_RU.UTF-8";
+    };
+  };
 
+  # X11 and Desktop Environment
   services.xserver = {
     enable = true;
-
     desktopManager = {
       xterm.enable = false;
     };
-   
     displayManager = {
-        defaultSession = "none+i3";
+      defaultSession = "none+i3";
     };
-
     windowManager.i3 = {
       enable = true;
       extraPackages = with pkgs; [
-        rofi # application launcher most people use
-        i3status # gives you the default i3 status bar
-        i3lock #default i3 screen locker
-        i3blocks #if you are planning on using i3blocks over i3status
+        rofi
+        i3status
+        i3lock
+        i3blocks
         dmenu
-     ];
+      ];
     };
   };
 
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-
-  # Enable sound with pipewire.
+  # Sound
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
@@ -87,27 +76,20 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
+    # jack.enable = true;
+    # media-session.enable = true;
   };
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
+  # Printing
+  services.printing.enable = true;
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-
+  # User Configuration
   users.defaultUserShell = pkgs.zsh;
-  
   users.users.serj = {
     isNormalUser = true;
     description = "serj";
     extraGroups = [ "networkmanager" "wheel" ];
     packages = with pkgs; [
-    #  thunderbird
       neofetch
       telegram-desktop
       mattermost-desktop
@@ -116,23 +98,19 @@
       git
       restic
       xfce.thunar
-        (vscode-with-extensions.override {
-          vscodeExtensions = with vscode-extensions; [
-            bbenoist.nix
-          ];
-        })
+      (vscode-with-extensions.override {
+        vscodeExtensions = with vscode-extensions; [
+          bbenoist.nix
+        ];
+      })
       steam
       unzip
       flameshot
       xfce.mousepad
-      #networkmanager-openconnect
-      #i3status
-      #i3blocks
     ];
   };
 
-  environment.shells = with pkgs; [ zsh ];
-
+  # Environment Variables
   environment.variables = rec {
     GIT_AUTHOR_NAME  = "Split174";
     GIT_AUTHOR_EMAIL = "sergei.popov174@gmail.com";
@@ -140,12 +118,13 @@
     GIT_COMMITTER_EMAIL = "sergei.popov174@gmail.com";
   };
 
+  # Shell Configuration
+  environment.shells = with pkgs; [ zsh ];
   programs.zsh = {
     enable = true;
     enableCompletion = true;
     autosuggestions.enable = true;
     syntaxHighlighting.enable = true;
-
     oh-my-zsh = {
       enable = true;
       plugins = [ "git" ];
@@ -153,53 +132,36 @@
     };
   };
 
+  # Program Configurations
+  programs = {
+    firefox.enable = true;
+    steam.enable = true;
+    thunar = {
+      enable = true;
+      plugins = with pkgs.xfce; [
+        # thunar-archive-plugin
+        # thunar-volman
+      ];
+    };
+  };
 
-  # Install firefox.
-  programs.firefox.enable = true;
-
-  programs.steam.enable = true;  
-
-  programs.thunar.enable = true;
-  programs.thunar.plugins = with pkgs.xfce; [
-    #thunar-archive-plugin
-    #thunar-volman
-  ];
-
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
+  # System Packages
   environment.systemPackages = with pkgs; [
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #  wget
-  #  vscode
-  #  i3status
+    # vim
+    # wget
+    # vscode
+    # i3status
   ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
+  # Optional Configurations (commented out)
+  # services.xserver.libinput.enable = true;
   # programs.mtr.enable = true;
   # programs.gnupg.agent = {
   #   enable = true;
   #   enableSSHSupport = true;
   # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
   # services.openssh.enable = true;
-
-  # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
   # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "24.05"; # Did you read the comment?
-
 }
