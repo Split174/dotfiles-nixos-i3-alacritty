f1b81735b05f77ffc4a33559302f9ca525af237a
Split174 <sergei.popov174@gmail.com>
2025-12-18 11:25:42 +0500

    add homepc id

--- hosts/modules/jobpc-syncthing.nix
+++ hosts/modules/jobpc-syncthing.nix
@@ -15,12 +15,12 @@
     settings = {
       devices = {
         "jipok-home-server" = { id = "PADCQLR-TRHLECY-KYSCMTV-W7ZJDJQ-PLQYCE4-E6BAR2P-QOBIRZF-5BD5IQ3"; };
-        "homepc" = { id = "DEVICE-ID-GOES-HERE"; };
+        "homepc" = { id = "KQQJDOX-UV5Z75P-RQATYSF-LWBBIUS-BLWXNUU-75OOY3I-SNRPBHB-PGMWVAY"; };
       };
       folders = {
         "Sync over Jipok homeserver" = {
           path = "/home/serj/sync-over-jipok-server";
-          devices = [ "jipok-home-server"];
+          devices = [ "jipok-home-server" "homepc"];
         };
       };
     };
